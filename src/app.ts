e8c3e845cceebf6e32e32134bb03da5a39bb50d3
jasleen kaur <jjasleenkaur2@rrc.ca>
2025-11-04 23:06:55 -0600

    Write Jest tests for custom claims functionality using mocked Firebase

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import {accessLogger,errorLogger,consoleLogger,} from "./api/v1/middleware/error
 import errorHandler from "./api/v1/middleware/errorHandler";
 import loanroutes from "./api/v1/routes/loanroutes";
 import userroutes from "./api/v1/routes/userroutes";
+import roleroutes from "./api/v1/routes/roleroutes";
 
 const app = express();
 
@@ -25,5 +26,8 @@ app.use("/api/v1",loanroutes);
 // Global error handling middleware (MUST be applied last)
 app.use(errorHandler);
 
+app.use("/api/v1/roles", roleroutes);
+
 app.use("/api/v1/users", userroutes);
+
 export default app;
